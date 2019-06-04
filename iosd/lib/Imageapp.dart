0d0d6e5de82134bba250c768dca89441ced7c730
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2019-06-04 23:10:20 +0530

    Inital commit

---
+++ iosd/lib/Imageapp.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Imageapp extends StatefulWidget {
+  @override
+  _ImageappState createState() => _ImageappState();
+}
+
+class _ImageappState extends State<Imageapp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ iosd/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:iosd/Imageapp.dart';
+void main(){
+  runApp(Imageapp());
+}
